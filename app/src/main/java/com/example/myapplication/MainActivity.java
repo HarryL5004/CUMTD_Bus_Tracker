bfced088414fefb6443b27c57fa6f7259d27fc6c
HarryL5004 <loklok8795@gmail.com>
2018-12-09 10:06:50 -0600

    UI

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        Button button = (Button) findViewById(R.id.search);
+
+        final EditText input = (EditText) findViewById(R.id.searchBox);
+        input.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_ATOP);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                textView.setText(input.getText());
+            }
+        });
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        return true;
+                    case R.id.navigation_busRoutes:
+                        Intent intent = new Intent(MainActivity.this, Bus_Routes.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.navigation_favorites:
+                        intent = new Intent(MainActivity.this, News.class);
+                        startActivity(intent);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
