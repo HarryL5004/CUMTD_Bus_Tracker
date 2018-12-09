cc7ee66065b3715d0b1b21fe13130a17c9ff9361
HarryL5004 <loklok8795@gmail.com>
2018-12-09 16:59:59 -0600

    Bus Tracker (main)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,32 +9,51 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private static RequestQueue requestQueue;
+    private TextView textView;
+    private EditText editText;
+    private String toDisplay = "";
+    private String stop_id = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        editText = (EditText) findViewById(R.id.searchBox);
+        editText.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_ATOP);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
         Button button = (Button) findViewById(R.id.search);
-
-        final EditText input = (EditText) findViewById(R.id.searchBox);
-        input.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_ATOP);
-
-        final TextView textView = (TextView) findViewById(R.id.textView);
-
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(final View v) {
-                textView.setText(input.getText());
+                editText.clearFocus();
+                editText.clearComposingText();
+                String input = editText.getText().toString();
+                getStopId(input);
             }
         });
 
@@ -57,5 +76,92 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    void getStopId(String stopName) {
+        stopName = stopName.toLowerCase().trim().replace(" and ", "+");
+        try {
+            final String url = "https://developer.cumtd.com/api/v2.2/json/getstopsbysearch?key=806cca7e36284b27ba31bf75ab5ee7a2&query=" + stopName;
+            JsonObjectRequest jsonObjectR = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Log.d("url", url);
+                                Log.d("Response1:", response.toString());
+                                idCallDone(response);
+                            } catch (Exception E) {
+                                Log.e("Exception", E.toString());
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Error Response", error.toString());
+                    textView.setText("Network Error");
+                }
+                });
+            requestQueue.add(jsonObjectR);
+        }
+        catch(Exception E) {
+            E.printStackTrace();
+        }
+    }
+    void idCallDone(JSONObject response) throws Exception {
+        JSONArray stopsArray = response.getJSONArray("stops");
+        JSONObject stopsObject = stopsArray.getJSONObject(0);
+        //textView.setText(stopsObject.getString("stop_name") + stopsObject.getString("stop_id"));
+        stop_id = stopsObject.getString("stop_id");
+        getDepartureTime(stop_id);
+    }
+    void getDepartureTime(String id) {
+        try {
+            Log.d("stop_id:::::", stop_id);
+            final String url = "https://developer.cumtd.com/api/v2.2/json/getdeparturesbystop?key=806cca7e36284b27ba31bf75ab5ee7a2&stop_id=" + id;
+            JsonObjectRequest jsonObjectR = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Log.d("url", url);
+                                Log.d("Response2: ", response.toString());
+                                callDone(response);
+                            } catch (Exception E) {
+                                Log.e("Exception", E.toString());
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Error Response", error.toString());
+                    textView.setText("Network Error");
+                }
+            });
+            requestQueue.add(jsonObjectR);
+        }
+        catch(Exception E) {
+            E.printStackTrace();
+        }
+    }
+    void callDone(JSONObject response) throws Exception{
+        JSONArray stopsArray = response.getJSONArray("departures");
+        JSONObject stopsObject;
+        JSONObject routeObject;
+        JSONObject tripObject;
+        for (int i = 0; i < stopsArray.length(); i++) {
+            stopsObject = stopsArray.getJSONObject(i);
+            routeObject = stopsObject.getJSONObject("route");
+            tripObject = stopsObject.getJSONObject("trip");
+            toDisplay += routeObject.getString("route_short_name") + " " + routeObject.getString("route_long_name") +
+                        " " + tripObject.getString("trip_headsign") + " Arriving in: " + stopsObject.getString("expected_mins") + "\n";
+        }
+        textView.setText(toDisplay);
     }
 }
