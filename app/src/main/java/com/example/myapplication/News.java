0651c142cc2827e6e201b213cfff8df08aec4836
HarryL5004 <loklok8795@gmail.com>
2018-12-09 13:09:47 -0600

    News Tab

--- app/src/main/java/com/example/myapplication/News.java
+++ app/src/main/java/com/example/myapplication/News.java
@@ -5,8 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,16 +21,34 @@ import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class News extends AppCompatActivity {
+    private static RequestQueue requestQueue;
+    private JsonParser parser = new JsonParser();
+    private JsonObject result;
+    private String toDisplay = "";
+    private TextView textView;
+    private TextView errorMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
-        final TextView textView = (TextView) findViewById(R.id.textView);
+        requestQueue = Volley.newRequestQueue(this);
+        textView = (TextView) findViewById(R.id.textView2);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        errorMsg = (TextView) findViewById(R.id.textView3);
+        apiCall();
+        final ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                apiCall();
+            }
+        });
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -51,7 +73,7 @@ public class News extends AppCompatActivity {
 
     void apiCall() {
         try {
-            String url = "https://developer.cumtd.com/api/2.2/json/getnews?key=806cca7e36284b27ba31bf75ab5ee7a2";
+            String url = "https://developer.cumtd.com/api/v2.2/json/getnews?key=806cca7e36284b27ba31bf75ab5ee7a2";
             JsonObjectRequest jsonObjectR = new JsonObjectRequest(
                     Request.Method.GET,
                     url,
@@ -59,23 +81,39 @@ public class News extends AppCompatActivity {
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
-                            callDone(response);
+                            errorMsg.setText("");
+                            try {
+                                callDone(response);
+                            } catch (Exception E) {
+                                Log.e("Exception", E.toString());
+                            }
                         }
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
+                            textView.setText("");
                             Log.e("Error Response", error.toString());
+                            errorMsg.setText("Network Error");
                         }
                     }
             );
+            requestQueue.add(jsonObjectR);
         }
         catch(Exception E) {
             E.printStackTrace();
         }
     }
 
-    void callDone(JSONObject response) {
+    void callDone(JSONObject response) throws Exception {
         Log.d("response", response.toString());
+        JSONArray newsArray = response.getJSONArray("news");
+        for (int i = 0; i < 10; i++) {
+            JSONObject newsObject = newsArray.getJSONObject(i);
+            toDisplay += "<h3>" + newsObject.getString("title") + "</h3>" +
+                    "<b>" + newsObject.getString("author") + "</b> " +
+                    newsObject.getString("postetd_date") + "<p>" + "    " + newsObject.getString("body") + "</p>";
+        }
+        textView.setText(Html.fromHtml(toDisplay));
     }
 
 }
